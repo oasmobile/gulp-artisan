08bd5f03b6443e9d99e0e58186800286c1a802ca
tangqiang <tangqiang@oasgames.com>
2017-03-07 18:39:19 +0800

    * css js 处理

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ sudo npm install --global gulp-oasis
 主命令 gulp-oasis  
 参数 --run compress 图片压缩  
 参数 --run atlas 生成精灵图  
+参数 --run minify_css CSS合并压缩  
+参数 --run minify_js JS合并压缩混淆  
 参数 --config xxxx.json 指定读取的配置文件  
 参数 --cwd 指定要读取配置的项目目录 不指定默认当前目录
 
@@ -29,7 +31,7 @@ compress默认配置文件范例compress_images.json
         "output": "dist/images2"
     }
 }
-atlas默认配置文件范例atlas_sprites.json 
+atlas默认配置文件范例atlas_sprites.json
 {
     "buttons1": {
         "input"     : "buttons1/*",
@@ -42,6 +44,40 @@ atlas默认配置文件范例atlas_sprites.json
         "output"    : "dist/buttons2",
         "image_name": "atlas_buttons2.png",
         "css_name"  : "atlas_buttons2.css"
+    }
+}
+minify_css默认配置文件范例minify_css.json
+{
+    "css1": {
+        "input" : "css1/*",
+        "output": "css1-minify"
+    },
+    "css2": {
+        "input" : "css2/*",
+        "output": "css2-minify",
+        "concat": "css2.min.css"
+    },
+    "css3": {
+        "input" : ["css3/a.css","css3/b.css"],
+        "output": "css3-minify",
+        "concat": "css3.min.css"
+    }
+}
+minify_js默认配置文件范例minify_js.json
+{
+    "js1": {
+        "input" : "js1/*",
+        "output": "js1-minify"
+    },
+    "js2": {
+        "input" : "js2/*",
+        "output": "js2-minify",
+        "concat": "js2.min.js"
+    },
+    "js3": {
+        "input" : ["js3/a.js","js3/b.js"],
+        "output": "js3-minify",
+        "concat": "js3.min.js"
     }
 }
 </pre>
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
 const gulp = require('gulp');
+
 const imagemin = require('gulp-imagemin');
 const spritesmith = require('gulp.spritesmith');
 const buffer = require('vinyl-buffer');
 const minimist = require('minimist');
-const fs = require('fs');
+
+const minifycss = require('gulp-minify-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 var argv = minimist(process.argv.slice(2));
 var cwd = argv.cwd;
@@ -46,6 +52,34 @@ else if (argv.run === 'atlas') {
 
     gulp.task('default', ['atlas']);
 }
+else if (argv.run === 'minify_css') {
+    checkConfigOption(argv.config);
+    var configCss = argv.config || 'minify_css.json';
+    try {
+        var configCssPath = cwd + '/' + configCss;
+        var configCssJson = JSON.parse(fs.readFileSync(configCssPath));
+    }
+    catch (e) {
+        console.log('默认配置文件未找到');
+        process.exit();
+    }
+
+    gulp.task('default', ['minify_css']);
+}
+else if (argv.run === 'minify_js') {
+    checkConfigOption(argv.config);
+    var configJs = argv.config || 'minify_js.json';
+    try {
+        var configJsPath = cwd + '/' + configJs;
+        var configJsJson = JSON.parse(fs.readFileSync(configJsPath));
+    }
+    catch (e) {
+        console.log('默认配置文件未找到');
+        process.exit();
+    }
+
+    gulp.task('default', ['minify_js']);
+}
 else {
     console.log('请输入正确的指令');
     process.exit();
@@ -86,5 +120,42 @@ gulp.task('atlas', function () {
         //生成对应的css
         var cssStream = spriteData.css
             .pipe(gulp.dest(dist));
+    }
+});
+
+//css处理
+gulp.task('minify_css', function () {
+    for (var cssNeedMinify in configCssJson) {
+        if (configCssJson[cssNeedMinify].concat === undefined) {
+            gulp.src(configCssJson[cssNeedMinify].input)
+                .pipe(minifycss())
+                .pipe(rename({suffix: '.min'}))
+                .pipe(gulp.dest(configCssJson[cssNeedMinify].output));
+        }
+        else {
+            gulp.src(configCssJson[cssNeedMinify].input)
+                .pipe(concat(configCssJson[cssNeedMinify].concat))
+                .pipe(minifycss())
+                .pipe(gulp.dest(configCssJson[cssNeedMinify].output));
+        }
+    }
+});
+
+//js处理
+gulp.task('minify_js', function () {
+    for (var jsNeedMinify in configJsJson) {
+        if (configJsJson[jsNeedMinify].concat === undefined) {
+            gulp.src(configJsJson[jsNeedMinify].input)
+                .pipe(rename({suffix: '.min'}))
+                .pipe(uglify({outSourceMap: true}))
+                .pipe(gulp.dest(configJsJson[jsNeedMinify].output))
+        }
+        else {
+            gulp.src(configJsJson[jsNeedMinify].input)
+                .pipe(concat(configJsJson[jsNeedMinify].concat))
+                .pipe(rename({suffix: '.min'}))
+                .pipe(uglify())
+                .pipe(gulp.dest(configJsJson[jsNeedMinify].output))
+        }
     }
 });
